9beb8ab023dc4cd6b1ff608dd87ebcbf8584a171
Peerakarn Jitpukdee <thanadule.k@siametrics.com>
2025-04-04 22:12:34 +0800

    modify config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,43 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-etherscan";
+import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+dotenvConfig();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL || "";
+
+const config = {
+  solidity: "0.8.22",
+  networks: {
+    // Local development network
+    hardhat: {},
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 84532,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  etherscan: {
+    apiKey: process.env.BASESCAN_API_KEY
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v5"
+  }
+} as HardhatUserConfig;
 
 export default config;
