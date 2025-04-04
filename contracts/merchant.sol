5e3e1fbbd41db90d49c8d3a220f9947366207ebb
Peerakarn Jitpukdee <thanadule.k@siametrics.com>
2025-04-04 22:30:44 +0800

    change contract name"
    "

--- contracts/merchant.sol
+++ contracts/merchant.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract BullshipAgent {
+contract AgentMerchant {
 
     struct AgentInfo {
       address walletAddress;
